8ac240d94c6d1004780737a09a602e28d4f6c94b
Joao-Pietro <nick.zin.izze@gmail.com>
2026-09-22 17:11:43 -0300

    Backend funcionando e inicio do Front

---
+++ Projeto/BackEnd/dist/route/userRoute.js
@@ -0,0 +1,5 @@
+import { Router } from "express";
+import { register } from "../controller/userController.js";
+const rota = Router();
+rota.post('/register', register);
+export default rota;
---
+++ Projeto/BackEnd/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors' //--> Ele envia os dados para o front poder usar
+import userRoute from "./route/userRoute.js"
+import loginRoute from "./route/loginRoute.js"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(userRoute)
+app.use(loginRoute)
+
+app.listen(3001, () => {
+  console.log(`Servidor rodando...Rodando em http://localhost:3001/`)
+})
+
+//Instalar o cors para enviar os dados para o front
+//npm install cors
+//npm install -D @types/cors
