9d6e38a22716d509e338d034e8281e453fcb8fe5
Danillo686 <uqualquer220@gmail.com>
2026-09-22 21:01:30 -0300

    Login e cadastro de cliente e empresa prontos

---
+++ Projeto/BackEnd/dist/utils/generateCode.js
@@ -0,0 +1,3 @@
+export function generateCode() {
+    return Math.floor(10000 + Math.random() * 900000).toString(); //Gera o código aleatorio após criar a conta, e devolve em string
+}
---
+++ Projeto/BackEnd/src/controller/loginController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express"
+import { supabase } from "../supabase.js"
+
+export async function login(
+    req: Request,
+    res: Response
+) {
+    const {email, password} = req.body
+
+    const {data, error} = await supabase.auth.signInWithPassword({email, password})
+
+    if (error) {
+        return res.status(401).json({Error: error.message})
+    }
+
+    const userId = data.user?.id
+
+    if (!userId) {
+        return res.status(400).json({message: `Não foi possível obter o ID do usuário`})
+    }
+
+    const { data: employee } = await supabase
+    .from("users")
+    .select("id, name, role")
+    .eq("id", userId)
+    .maybeSingle()
+
+    if (employee) {
+        return res.status(200).json({message:"Login realizado com sucesso", user: employee, session: data.session})
+    }
+
+    const {data: customer} = await supabase
+    .from('customer')
+    .select('id, name, role')
+    .eq("id", userId)
+    .maybeSingle()
+
+    if (customer) {
+        return res.status(200).json({message:"Login realizado com sucesso", user:customer, session: data.session})
+    }
+
+    return res.status(404).json({message: "Perfil do usuário não encontrado"})
+}
+
+
+
+
+
